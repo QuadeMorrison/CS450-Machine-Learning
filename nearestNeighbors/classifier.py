a4ae877b4c094ea80e37c58d5083c480990c2d74
Quade Morrison <quademorrison@gmail.com>
2017-01-19 20:53:22 -0700

    Completed Nearest Neighbors algorithm

--- nearestNeighbors/classifier.py
+++ nearestNeighbors/classifier.py
@@ -1,7 +1,19 @@
+from numpy import argsort, argmax
+
+def knn(k, train_data, train_targets, test_instance):
+    distances = ((train_data - test_instance) ** 2).sum(axis = 1)
+    neighbors = [train_targets[i] for i in argsort(distances)[:k]]
+    return max(set(neighbors), key=neighbors.count)
+
 class Classifier(object):
 
+    def __init__(self, k=3):
+        self.k = k
+
     def fit(self, dataset, targets):
-        pass
+        self.train_data = dataset
+        self.train_targets = targets
 
     def predict(self, dataset):
-        return [x*0 for x in range(len(dataset))]
+        return [knn(self.k, self.train_data, self.train_targets, instance)
+                for instance in dataset]
